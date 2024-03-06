20b08f5b0518a566d84965d29bfd31f4e52f37ca
Gilbertfritzzz <159890631+Gilbertfritzzz@users.noreply.github.com>
2024-03-07 00:27:14 +0700

    Update Proyek_Analisis_Data.py

--- Proyek_Analisis_Data.py
+++ Proyek_Analisis_Data.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load data
-day_df = pd.read_csv('/day.csv')
-hour_df = pd.read_csv('/hour.csv')
+day_df = pd.read_csv('day.csv')
+hour_df = pd.read_csv('hour.csv')
 
 # Plot 1: Hourly Rental
 rent_hr = hour_df.groupby('hr')['cnt_hour'].mean()
