e2b0c1cb689bafe35d4e720966e3453b71b6b194
Gilbertfritzzz <159890631+Gilbertfritzzz@users.noreply.github.com>
2024-03-07 13:08:25 +0700

    Update Proyek_Analisis_Data.py

--- Proyek_Analisis_Data.py
+++ Proyek_Analisis_Data.py
@@ -5,33 +5,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load data
-day_df = pd.read_csv('day.csv')
-hour_df = pd.read_csv('hour.csv')
+day_df = pd.read_csv('/day.csv')
+hour_df = pd.read_csv('/hour.csv')
 
-#merge
+# merge
 bike_df = hour_df.merge(day_df, on='dteday', how='inner', suffixes=('_hour', '_day'))
-bike_df.head()
 
 # Plot 1: Hourly Rental
 rent_hr = bike_df.groupby('hr')['cnt_hour'].mean()
 
-plt.bar(rent_hr.index, rent_hr.values)
+# Streamlit code for Plot 1
+st.bar_chart(rent_hr)
 
-plt.title('Rata - Rata Penyewaan Sepeda per Jam')
-plt.xlabel('Jam')
-plt.ylabel('Rata - Rata Penyewaan')
-
-plt.show()
+st.title('Rata - Rata Penyewaan Sepeda per Jam')
+st.xlabel('Jam')
+st.ylabel('Rata - Rata Penyewaan')
 
 # Plot 2: Daily Rental on Holidays
 avg_holiday = bike_df.groupby('holiday_day')['cnt_day'].mean().reset_index().sort_values("cnt_day")
 
-plt.figure(figsize=(8, 5))
-sns.barplot(x='holiday_day', y='cnt_day', data=avg_holiday, palette='Set1')
+# Streamlit code for Plot 2
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.barplot(x='holiday_day', y='cnt_day', data=avg_holiday, palette='Set1', ax=ax)
 
-plt.title('Rata-rata Penyewaan Sepeda pada Hari Libur')
-plt.xlabel('Hari Libur')
-plt.ylabel('Rata-rata Penyewaan')
-plt.xticks([0, 1], ['Tidak Libur', 'Libur'])
+st.pyplot(fig)
 
-plt.show()
+st.title('Rata-rata Penyewaan Sepeda pada Hari Libur')
+st.xlabel('Hari Libur')
+st.ylabel('Rata-rata Penyewaan')
+st.xticks([0, 1], ['Tidak Libur', 'Libur'])
